68d852284abbc87de52f10eaab42321a8a399465
Burak Eren Mert <buramert@k1m30s01.42istanbul.com.tr>
2025-02-27 20:00:40 +0300

    final commit

--- radix.c
+++ radix.c
@@ -3,46 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   radix.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: burakerenmert <burakerenmert@student.42    +#+  +:+       +#+        */
+/*   By: buramert <buramert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 03:26:23 by burakerenme       #+#    #+#             */
-/*   Updated: 2025/02/26 04:48:21 by burakerenme      ###   ########.fr       */
+/*   Updated: 2025/02/27 19:57:54 by buramert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// Finds the largest index in the stack
-int find_max_index(stack *a) {
-    int max = a->index;
-    while (a) {
-        if (a->index > max)
-            max = a->index;
-        a = a->next;
-    }
-    return max;
+int	find_max_index (stack *a)
+{
+	int max = a->index;
+	while (a)
+	{
+		if (a->index > max)
+			max = a->index;
+		a = a->next;
+	}
+	return max;
 }
-
-// Calculates the number of bits needed for the largest index
 int calculate_max_bits(int max) {
-    int bits = 0;
-    while ((max >> bits) != 0)
-        bits++;
-    return bits;
+	int bits = 0;
+	while ((max >> bits) != 0)
+		bits++;
+	return (bits);
 }
-
-// Radix sort implementation using indexes
 void radix_sort(stack **a, stack **b) {
-    if (!a || !*a) {
-        printf("Stack A is empty!\n");
-        return;
-    }
+	if (!a || !*a)
+		return;
+	int max = find_max_index(*a);  // Find the largest index
+	int max_bits = calculate_max_bits(max);  // Calculate the number of bits required
+	int i = 0;
 
-    int max = find_max_index(*a);  // Find the largest index
-    int max_bits = calculate_max_bits(max);  // Calculate the number of bits required
-    int i = 0;
-
-    while (i < max_bits) {
+	while (i < max_bits)
+	{
         int size = max + 1;  // Number of elements in stack A is max index + 1
         int j = 0;
 
@@ -64,4 +59,3 @@ void radix_sort(stack **a, stack **b) {
         i++;
     }
 }
-
